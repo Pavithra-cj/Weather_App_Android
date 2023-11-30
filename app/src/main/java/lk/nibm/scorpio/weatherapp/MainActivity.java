ca260107baaf6932cb05964362a0f02ab9a85be3
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2023-11-30 11:37:29 +0530

    UI done and Tested.

--- app/src/main/java/lk/nibm/scorpio/weatherapp/MainActivity.java
+++ app/src/main/java/lk/nibm/scorpio/weatherapp/MainActivity.java
@@ -22,10 +22,13 @@ import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
+import lk.nibm.scorpio.weatherapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     EditText etCity, etCountry;
-    TextView tvResult;
     private final String url = "https://api.openweathermap.org/data/2.5/weather";
     private final String appid = "bb9a7e721f24b3de439f438c37e6aa00";
     DecimalFormat df = new DecimalFormat("#.##");
@@ -33,12 +36,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        etCity = findViewById(R.id.etCity);
-        etCountry = findViewById(R.id.etCountry);
-        tvResult = findViewById(R.id.tvResult);
+        etCity = binding.etCity;
+        etCountry = binding.etCountry;
+    }
 
+    private void showToast(String message)
+    {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void imageView(String weather)
+    {
+        if (weather.equals("Rain"))
+        {
+            binding.image.setImageResource(R.drawable.rain);
+        } else if (weather.equals("Clouds")) {
+            binding.image.setImageResource(R.drawable.cloudy);
+        } else if (weather.equals("Cloudy Sunny")) {
+            binding.image.setImageResource(R.drawable.cloudy_sunny);
+        } else if (weather.equals("Rainy")) {
+            binding.image.setImageResource(R.drawable.rainy);
+        } else if (weather.equals("Snow")) {
+            binding.image.setImageResource(R.drawable.snowy);
+        } else if (weather.equals("Thunderstorm")) {
+            binding.image.setImageResource(R.drawable.storm);
+        } else if (weather.equals("Clear")) {
+            binding.image.setImageResource(R.drawable.sunny);
+        } else if (weather.equals("Windy")) {
+            binding.image.setImageResource(R.drawable.windy);
+        }
     }
 
     public void getWeatherDetails(View view) {
@@ -46,12 +77,12 @@ public class MainActivity extends AppCompatActivity {
         String city = etCity.getText().toString().trim();
         String country = etCountry.getText().toString().trim();
         if(city.equals("")){
-            tvResult.setText("City field can not be empty!");
+            showToast("City cannot be empty!");
         }else{
             if(!country.equals("")){
-                tempUrl = url + "?q=" + city + "," + country + "&appid=" + appid;
+                tempUrl = url + "?q=" + city + "," + country + "&appid=" + appid + "&units=metric";
             }else{
-                tempUrl = url + "?q=" + city + "&appid=" + appid;
+                tempUrl = url + "?q=" + city + "&appid=" + appid + "&units=metric";
             }
             StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
                 @Override
@@ -61,29 +92,28 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject jsonResponse = new JSONObject(response);
                         JSONArray jsonArray = jsonResponse.getJSONArray("weather");
                         JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String weather = jsonObjectWeather.getString("main");
+                        imageView(weather);
+                        binding.infoMain.setText(weather);
                         String description = jsonObjectWeather.getString("description");
                         JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
-                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
-                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        double temp = jsonObjectMain.getDouble("temp");
+                        binding.degreeMain.setText(String.valueOf(temp) + "°C");
+                        double feelsLike = jsonObjectMain.getDouble("feels_like");
+                        binding.seaGroundLevel.setText(String.valueOf(feelsLike) + "°C");
                         float pressure = jsonObjectMain.getInt("pressure");
+                        binding.barometerMain.setText(String.valueOf(pressure) + "%");
                         int humidity = jsonObjectMain.getInt("humidity");
+                        binding.humidityMain.setText(String.valueOf(humidity) + "%");
                         JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
                         String wind = jsonObjectWind.getString("speed");
+                        binding.windMain.setText(wind + "m/s");
                         JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
                         String clouds = jsonObjectClouds.getString("all");
                         JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
                         String countryName = jsonObjectSys.getString("country");
                         String cityName = jsonResponse.getString("name");
-                        tvResult.setTextColor(Color.rgb(68,134,199));
-                        output += "Current weather of " + cityName + " (" + countryName + ")"
-                                + "\n Temp: " + df.format(temp) + " °C"
-                                + "\n Feels Like: " + df.format(feelsLike) + " °C"
-                                + "\n Humidity: " + humidity + "%"
-                                + "\n Description: " + description
-                                + "\n Wind Speed: " + wind + "m/s (meters per second)"
-                                + "\n Cloudiness: " + clouds + "%"
-                                + "\n Pressure: " + pressure + " hPa";
-                        tvResult.setText(output);
+                        binding.city.setText(cityName);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
